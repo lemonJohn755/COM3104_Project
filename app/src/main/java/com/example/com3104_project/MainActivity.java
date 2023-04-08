336d9970b3d5bbb99c513cef8e4c9d2398d5b240
John Chan WK <chan.wingkin9287@gmail.com>
2023-04-08 19:06:22 +0800

    add Google map display in MainActivity.java & activity_main.xml

--- app/src/main/java/com/example/com3104_project/MainActivity.java
+++ app/src/main/java/com/example/com3104_project/MainActivity.java
@@ -1,14 +1,170 @@
 package com.example.com3104_project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import com.example.com3104_project.databinding.ActivityMainBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+
+    private GoogleMap mMap;
+    private ActivityMainBinding binding;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters // The minimum time between updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 200 * 10 * 1; // 2 seconds Location location;
+    private static final int REQUEST_CODE = 100;
+    protected LocationManager locationManager;
+    private Location location;
+
+
+    SupportMapFragment mapFragment;
+    double myLat = 0;
+    double myLon = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(MainActivity.this);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        getLastLocation();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE){
+            if((grantResults.length >0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)){
+                getLastLocation();
+            }
+        }else{
+            Toast.makeText(MainActivity.this, "permission not granted", Toast.LENGTH_LONG).show();
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void getLastLocation() {
+        locationManager = (LocationManager) MainActivity.this.getSystemService(LOCATION_SERVICE);
+        boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        if (!isGPSEnabled && !isNetworkEnabled) {
+            // no network provider is enabled
+            Log.d("Network-GPS", "Disable");
+            Toast.makeText(MainActivity.this, "No provider enabled", Toast.LENGTH_SHORT).show();
+        } else {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                askPermission();
+                return;
+            }
+            Log.d("Network-GPS", "isNetworkEnabled: "+isNetworkEnabled);
+            if (isNetworkEnabled) {
+                // get loc update
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                        MIN_TIME_BW_UPDATES,
+                        MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                        (LocationListener) this);
+                location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if (location != null){
+                    myLat = location.getLatitude();
+                    myLon = location.getLongitude();
+                    Log.d("location","GPS Provider location obtained, lat "+myLat+" lon "+myLon);
+                    Toast.makeText(MainActivity.this, "GPS Provider location obtained", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    private void askPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+        Log.d("location", "asking for permission...");
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker in Sydney and move the camera
+        LatLng myLocation = new LatLng(myLat, myLon);
+        Log.d("location", "showing map now "+ myLat+"::"+myLon);
+
+        String addr = reverseGeoCode(myLat, myLon);         // Convert lan lon to address
+
+        mMap.addMarker(new MarkerOptions().position(myLocation).title(addr));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLocation,15));
+    }
+
+    public String reverseGeoCode(double lat, double lon){
+        String addressText = "";
+
+        try {
+            Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(lat, lon, 1);
+            if (addresses.size() > 0) {
+                Address address = addresses.get(0);
+                addressText = address.getAddressLine(0);
+                Log.d("reverseGeoCode", "Address found:\n" + addressText);
+                return addressText;
+            } else {
+                Log.d("reverseGeoCode", "Address not found, lan=" + lat + " lon" + lon);
+                return "Address not found, lan=" + lat + " lon" + lon;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.d("reverseGeoCode", e.toString());
+        }
+        return "Address not found, lan=" + lat + " lon" + lon;
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
     }
 }
